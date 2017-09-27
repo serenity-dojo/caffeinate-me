22794c43fdd16c536aa6a879aad358c5f8349a5d
John Ferguson Smart <john.smart@wakaleo.com>
2017-09-27 17:30:42 +0100

    Preparation for lesson 4

--- src/test/java/caffeinateme/OrderCoffeeSteps.java
+++ src/test/java/caffeinateme/OrderCoffeeSteps.java
@@ -1,6 +1,5 @@
 package caffeinateme;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -15,26 +14,25 @@ public class OrderCoffeeSteps {
 
     String cathysOrder;
 
-    @Given("^Cathy is (\\d+) meters from the coffee shop$")
-    public void cathy_is_n_meters_from_the_coffee_shop(int distanceInMeters) throws Throwable {
+    @Given("(.*) is (\\d+) meters? from the coffee shop")
+    public void cathy_is_n_meters_from_the_coffee_shop(String name, int distanceInMeters) throws Throwable {
+        System.out.println("Meters = " + distanceInMeters);
         cathy.setDistanceFromShop(distanceInMeters);
     }
 
-    @When("^Cathy orders a (.*)$")
+    @When("Cathy (?:orders|has ordered) a (.*)$")
     public void cathy_orders_a(String order) throws Throwable {
         cathysOrder = order;
         cathy.placesOrderFor(cathysOrder);
     }
 
-    @Then("^Barry should receive the order$")
+    @Then("Barry should receive the order$")
     public void barry_should_receive_the_order() throws Throwable {
         assertThat(barry.getPendingOrders(), hasItem(cathysOrder));
     }
 
     @Then("^Barry should know that the coffee is Urgent$")
     public void barry_should_know_that_the_coffee_is_Urgent() throws Throwable {
-
-
         assertThat(barry.getUrgentOrders(), hasItem(cathysOrder));
     }
 
