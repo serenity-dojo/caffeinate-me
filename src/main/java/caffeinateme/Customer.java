ce2c0242efa1c65a2b74493e72d805e9af94b61a
John Ferguson Smart <john.smart@wakaleo.com>
2017-10-12 14:10:17 +0100

    Solution to lesson 4

--- src/main/java/caffeinateme/Customer.java
+++ src/main/java/caffeinateme/Customer.java
@@ -5,10 +5,12 @@ import net.thucydides.core.annotations.Step;
 public class Customer {
     private int distanceFromShop;
 
-    public void setDistanceFromShop(int distanceFromShop) {
+    @Step("Notify caffeinate me that the customer is {0} metres from the shop")
+    public void notifyDistanceFromShop(int distanceFromShop) {
         this.distanceFromShop = distanceFromShop;
     }
 
+    @Step
     public void placesOrderFor(String order) {
 
     }
