10a1693e269835f08fb4808832356de8afbfb079
John Ferguson Smart <john.smart@wakaleo.com>
2020-03-28 17:23:35 +0000

    Sample solution

--- src/main/java/caffeinateme/model/CoffeeShop.java
+++ src/main/java/caffeinateme/model/CoffeeShop.java
@@ -7,8 +7,10 @@ public class CoffeeShop {
     private Queue<Order> orders = new LinkedList<>();
 
     public void placeOrder(Order order, int distanceInMetres) {
+        if (distanceInMetres <= 200) {
+            order = order.withStatus(OrderStatus.Urgent);
+        }
         orders.add(order);
-
     }
 
     public List<Order> getPendingOrders() {
