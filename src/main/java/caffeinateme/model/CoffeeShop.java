7f621d861a22fc3a7020ddeb5b26a5677c9c8a11
John Ferguson Smart <john.smart@wakaleo.com>
2020-03-29 22:16:45 +0100

    Updated domain logic for exercise 7

--- src/main/java/caffeinateme/model/CoffeeShop.java
+++ src/main/java/caffeinateme/model/CoffeeShop.java
@@ -1,11 +1,17 @@
 package caffeinateme.model;
 
-import java.nio.file.attribute.FileTime;
+import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class CoffeeShop {
 
     private Queue<Order> orders = new LinkedList<>();
+    private final ProductCatalog productCatalog;
+
+    public CoffeeShop(ProductCatalog productCatalog) {
+        this.productCatalog = productCatalog;
+    }
 
     public void placeOrder(Order order, float distanceInMetres) {
         if (distanceInMetres <= 200) {
@@ -37,4 +43,35 @@ public class CoffeeShop {
                 }
         );
     }
+
+    public Receipt getReceiptFor(Customer customer) {
+        List<Order> customerOrders = orders.stream()
+                .filter( order -> order.getCustomer().equals(customer))
+                .collect(Collectors.toList());
+
+        double subTotal = customerOrders.stream()
+                .mapToDouble(this::subtotalFor)
+                .sum();
+
+        List<ReceiptLineItem> lineItems = customerOrders.stream()
+                .map(order -> new ReceiptLineItem(order.getProduct(), order.getQuantity(), subtotalFor(order)))
+                .collect(Collectors.toList());
+
+        double serviceFee = subTotal * 5 / 100;
+        double total = subTotal + serviceFee;
+        return new Receipt(roundedTo2DecimalPlaces(subTotal),
+                roundedTo2DecimalPlaces(serviceFee),
+                roundedTo2DecimalPlaces(total),
+                lineItems);
+    }
+
+    private double roundedTo2DecimalPlaces(double value) {
+        return new BigDecimal(Double.toString(value)).setScale(2, BigDecimal.ROUND_HALF_UP)
+                .doubleValue();
+    }
+
+    private double subtotalFor(Order order) {
+        return productCatalog.priceOf(order.getProduct()) * order.getQuantity();
+    }
+
 }
